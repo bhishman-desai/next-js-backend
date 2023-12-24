b1f1a6739fe5a3565a6a32f0cb585fc429289727
Bhishman <bhishman@dal.ca>
2023-12-24 19:35:19 -0400

    Refactoring (fetching id for dynamic routes in a next.js manner)

--- src/app/api/todos/[todoID]/route.ts
+++ src/app/api/todos/[todoID]/route.ts
@@ -2,10 +2,14 @@ import { NextResponse } from "next/server";
 
 const DATA_SOURCE_URL: string = "https://jsonplaceholder.typicode.com/todos";
 
-export const GET = async (request: Request) => {
-  const id = request.url.split("/").at(-1) as string;
+type Props = {
+  params: {
+    todoID: string;
+  };
+};
 
-  const response: Response = await fetch(`${DATA_SOURCE_URL}/${id}`);
+export const GET = async (request: Request, { params: { todoID } }: Props) => {
+  const response: Response = await fetch(`${DATA_SOURCE_URL}/${todoID}`);
   const data: TODO = await response.json();
 
   if (!data.id) return NextResponse.json({ message: `TODO Not Found!` });
