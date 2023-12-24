1c302b3093e54050855885dade80ab8cd6713b1b
Bhishman <bhishman@dal.ca>
2023-12-24 16:55:36 -0400

    Adding the POST call

---
+++ src/app/api/feedback/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+type Feedback = {
+  name?: string;
+  email?: string;
+  message?: string;
+};
+
+export const POST = async (request: Request) => {
+  const data: Feedback = await request.json();
+
+  const { name, email, message } = data;
+
+  return NextResponse.json({ name, email, message });
+};
