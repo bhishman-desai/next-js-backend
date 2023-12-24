caa8aae1afd580930ac6779a3579d87dbe3f8f1a
Bhishman <bhishman@dal.ca>
2023-12-24 16:26:36 -0400

    Accessing the dynamic parameters from the URL

---
+++ src/app/api/echo/route.ts
@@ -0,0 +1,11 @@
+import React from "react";
+import { NextResponse } from "next/server";
+
+export const GET = async (request: Request) => {
+  const { searchParams } = new URL(request.url);
+
+  const name = searchParams.get("name");
+  const course = searchParams.get("course");
+
+  return NextResponse.json({ name, course });
+};
